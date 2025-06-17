49b808b89f19247cb832a9ca73459ad9a846e925
Selina <selinaxue@Selinas-MacBook-Pro.local>
2025-06-17 13:50:45 -0700

    Added Lab 2

---
+++ lab2.py
@@ -0,0 +1,12 @@
+name = 'Selina Xue'
+
+nickname = "Selin"
+
+age = 18
+
+used_python = False
+
+hobbies = ["Origami", "Reading Books", "Playing Music", "Listening to Ted Talks"]
+
+favorite_things = {"Manga":"Welcome to Demon School Iruma Kun",'Food': 'Sashimi', 'Color': 'Pink', 'Game': 'Sudoku', 'Genre': 'Fantasy' }
+
